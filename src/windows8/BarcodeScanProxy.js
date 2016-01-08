65bce2e5f827c925fea2cbb8eaf65005831955c7
Joel Braga <joelbraga.dev@gmail.com>
2016-01-08 17:49:57 +0000

    <fix>(src): Remove define from code

--- src/windows8/BarcodeScanProxy.js
+++ src/windows8/BarcodeScanProxy.js
@@ -1,36 +1,31 @@
-cordova.define("com.tlantic.plugins.device.barcodescan.BarcodeScanProxy", function(require, exports, module) {     /* global console, exports, require */
-    'use strict';
+'use strict';
 
-    var BarcodeScan = require('com.tlantic.plugins.device.barcodescan.BarcodeScan');
+var BarcodeScan = require('com.tlantic.plugins.device.barcodescan.BarcodeScan');
 
-    var barcodeScan;
+var barcodeScan;
 
-    exports.init = function (success, fail, args){
+exports.init = function (success, fail, args) {
 
-        if (!barcodeScan) {
-            barcodeScan = new BarcodeScan();
+    if (!barcodeScan) {
+        barcodeScan = new BarcodeScan();
+        barcodeScan.onReceive = exports.rcMessage;
+        barcodeScan.init(success, fail);
+    }
+    else {
+        barcodeScan.endReceivingData(function () {
             barcodeScan.onReceive = exports.rcMessage;
             barcodeScan.init(success, fail);
-        }
-        else {
-            barcodeScan.endReceivingData(function () {
-                barcodeScan.onReceive = exports.rcMessage;
-                barcodeScan.init(success, fail);
-            });
-        }
+        });
+    }
+};
 
+exports.stop = function stop(success, fail, args) {
+    barcodeScan.endReceivingData(success);
+};
 
-    };
+// callback to receive data written on socket inputStream
+exports.rcMessage = function (scanLabel, scanData, scanType) {
+    window.tlantic.plugins.device.barcodescan.receive(scanLabel, scanData, scanType);
+};
 
-    exports.stop = function stop(success, fail, args){
-        barcodeScan.endReceivingData(success);
-    };
-
-    // callback to receive data written on socket inputStream
-    exports.rcMessage = function (scanLabel, scanData, scanType) {
-        window.tlantic.plugins.device.barcodescan.receive(scanLabel, scanData, scanType);
-    };
-
-    require('cordova/windows8/commandProxy').add('DeviceBarcodeScan', exports);
-
-});
+require('cordova/windows8/commandProxy').add('DeviceBarcodeScan', exports);
